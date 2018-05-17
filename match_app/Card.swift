4b92860e6cd66fa46c703d76f80c84cc1509e57c
student33 <student33@student33.local>
2018-05-17 20:50:38 +0900

    Lce2

---
+++ match_app/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  match_app
+//
+//  Created by student33 on 2018. 5. 17..
+//  Copyright © 2018년 student33. All rights reserved.
+//
+
+import Foundation
---
+++ match_app/CardColeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CardColeCollectionViewCell.swift
+//  match_app
+//
+//  Created by student33 on 2018. 5. 17..
+//  Copyright © 2018년 student33. All rights reserved.
+//
+
+import UIKit
+
+class CardColeCollectionViewCell: UICollectionViewCell {
+    
+}
