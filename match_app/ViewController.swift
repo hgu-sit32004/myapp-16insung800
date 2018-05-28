29522f598e288794498de4aa7d41dbb388d6da29
student33 <student33@student33.local>
2018-05-28 20:24:55 +0900

    lec5

--- match_app/ViewController.swift
+++ match_app/ViewController.swift
@@ -8,13 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     var model = CardModel()
     var cardArray = [Card]()
     
+    @IBOutlet weak var CollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        CollectionView.delegate = self
+        CollectionView.dataSource = self
         
         // Call the getCards method of the card model
         cardArray = model.getCard()
@@ -25,6 +29,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: -UICollectionviw Protocol Methods
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+    
+        return cardArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
 
 }
+
+
 
